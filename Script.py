b0ef7c53ad39b8b94428987b8cb247a2990fc207
xeroxbayzid <134350713+xeroxbayzid@users.noreply.github.com>
2024-10-19 17:27:32 +0600

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,7 +4,19 @@
 
 
 class script(object):
-    START_TXT = """<b>Hᴇʟʟᴏ {},ᴍʏ ɴᴀᴍᴇ {},【ɪ ᴀᴍ ʟᴀᴛᴇꜱᴛ ᴀᴅᴠᴀɴᴄᴇᴅ】ᴀɴᴅ ᴘᴏᴡᴇʀꜰᴜʟ ꜰɪʟᴇ ꜱᴛᴏʀᴇ ʙᴏᴛ +└ᴄʟᴏɴᴇ ғᴇᴀᴛᴜʀᴇ┘+└sᴛʀᴇᴀᴍ / ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ ғᴇᴀᴛᴜʀᴇ┘+├ᴄᴜꜱᴛᴏᴍ ᴜʀʟ ꜱʜᴏʀᴛɴᴇʀ ꜱᴜᴘᴘᴏʀᴛ┤+  ᢵᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ sᴜᴘᴘᴏʀᴛ ᢴ ᢾᴀɴᴅ ʙᴇꜱᴛ ᴜɪ ᴘᴇʀꜰᴏʀᴍᴀɴᴄᴇᢿ</b>"""
+    START_TXT = """<b><u>🫡 Assalamu Alaikum -!!💝💫</u></b>
+
+<b><u>🤖 I'm Latest Advanced & Powerful File Store Bot-!!🔥</u></b>
+
+<b><u>📝 Features :</u></b>
+<b>⦿ Clone Feature</b>
+<b>⦿ Stream / Download Link</b>
+<b>⦿ Custom Url Shortner</b>
+<b>⦿ Photo Delete Support</b>
+<b>⦿ Best Ui Performance</b>
+<b>⦿ Permanent Link</b>
+
+<b><u>🤖 How To Use Me :</u></b> <b>You Can Send Any Telegram Document & The Bot Will Generate a Link, Just Open The Link & Click On Start Button The Bot Will Provide Your Document…! 📃</b>"""
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
@@ -35,9 +47,7 @@ If You Want To Remove Api Then Copy This And Send To Bot - `/api None`"""
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-    CLONE_START_TXT = """<b>Hᴇʟʟᴏ {},ᴍʏ ɴᴀᴍᴇ {},【ɪ ᴀᴍ ʟᴀᴛᴇꜱᴛ ᴀᴅᴠᴀɴᴄᴇᴅ】ᴀɴᴅ ᴘᴏᴡᴇʀꜰᴜʟ ꜰɪʟᴇ ꜱᴛᴏʀᴇ ʙᴏᴛ +├ᴄᴜꜱᴛᴏᴍ ᴜʀʟ ꜱʜᴏʀᴛɴᴇʀ ꜱᴜᴘᴘᴏʀᴛ┤+  ᢵᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ sᴜᴘᴘᴏʀᴛ ᢴ ᢾᴀɴᴅ ʙᴇꜱᴛ ᴜɪ ᴘᴇʀꜰᴏʀᴍᴀɴᴄᴇᢿ
-
-ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛʜɪs ғᴇᴀᴛᴜʀᴇ ᴛʜᴇɴ ᴄʀᴇᴀᴛᴇ ʏᴏᴜʀ ᴏᴡɴ ᴄʟᴏɴᴇ ʙᴏᴛ ғʀᴏᴍ ᴍʏ <a href=https://t.me/vj_botz>ᴘᴀʀᴇɴᴛ</a></b>"""
+    CLONE_START_TXT = """<b><u>🫡 Assalamu Alaikum {}-!!💝💫</u></b><b><u>🤖 I'm Latest Advanced & Powerful File Store Bot-!!🔥</u></b><b><u>📝 Features :</u></b><b>⦿ Clone Feature</b><b>⦿ Stream / Download Link</b><b>⦿ Custom Url Shortner</b><b>⦿ Photo Delete Support</b><b>⦿ Best Ui Performance</b><b>⦿ Permanent Link</b><b><u>🤖 How To Use Me :</u></b> <b>You Can Send Any Telegram Document & The Bot Will Generate a Link, Just Open The Link & Click On Start Button The Bot Will Provide Your Document…! 📃</b>"""
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
